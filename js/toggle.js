7333af504ce83fa2a232c3e1e922d2a06ea5e5f9
Frudrax Cheng <i@cynosura.one>
2023-09-02 00:00:03 +0800

    improved mobile devices layout

--- js/toggle.js
+++ js/toggle.js
@@ -1,9 +1,4 @@
-document.querySelectorAll(".toggleButton").forEach(function(button) {
-    button.addEventListener("click", function() {
-        var toggleInfoDiv = button.closest(".toggle-info-div");
-        var contentDiv = toggleInfoDiv.nextElementSibling;
-        
-        toggleInfoDiv.classList.add("hidden");
-        contentDiv.classList.remove("hidden");
-    });
+document.querySelector(".toggleButton").addEventListener("click", function() {
+    var contentDiv = document.querySelector(".contentDiv");
+    contentDiv.classList.toggle("hidden");
 });
