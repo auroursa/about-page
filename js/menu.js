1fa3e40a2c46e931639be5d4275d4decd5103066
Frudrax Cheng <i@cynosura.one>
2023-09-06 14:31:25 +0800

    try to fix Safari on iOS scrolling issue#3

--- js/menu.js
+++ js/menu.js
@@ -2,15 +2,13 @@ const menu = document.querySelector('.menu-div');
 let scrollTimer;
 
 window.addEventListener('scroll', () => {
-    const rect = menu.getBoundingClientRect();
-    if (rect.top === 0) {
-        menu.classList.add('at-top');
-    } else {
-        menu.classList.remove('at-top');
-    }
-
     clearTimeout(scrollTimer);
-
-    scrollTimer = setTimeout(function() {
-    }, 120);
+    scrollTimer = setTimeout(() => {
+        const rect = menu.getBoundingClientRect();
+        if (rect.top === 0) {
+            menu.classList.add('at-top');
+        } else {
+            menu.classList.remove('at-top');
+        }
+    }, 200);
 });
