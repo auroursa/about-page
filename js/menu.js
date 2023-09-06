5516038761470fac15225114cfddf7b353dfa891
Frudrax Cheng <i@cynosura.one>
2023-09-06 15:46:38 +0800

    removed UA detection method in menu.js

--- js/menu.js
+++ js/menu.js
@@ -18,24 +18,7 @@ function checkScroll() {
 
 function onScroll() {
     clearTimeout(scrollTimeout);
-    scrollTimeout = setTimeout(checkScroll, 100); // 设置延迟时间等待滚动停止
+    scrollTimeout = setTimeout(checkScroll, 120);
 }
 
-function onTouchMove() {
-    clearTimeout(scrollTimeout); // 用户滚动时清除计时器
-}
-
-function onTouchEnd() {
-    checkScroll(); // 在touchend事件中进行滚动检测
-}
-
-function isIOS() {
-    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-}
-
-if (isIOS()) {
-    window.addEventListener('touchmove', onTouchMove);
-    window.addEventListener('touchend', onTouchEnd);
-} else {
-    window.addEventListener('scroll', onScroll); // 在其他平台上使用scroll事件
-}
+window.addEventListener('scroll', onScroll);
