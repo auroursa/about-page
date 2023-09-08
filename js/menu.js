6d65a77fc464864e283f39f70c087f16bc5c71a4
Frudrax Cheng <i@cynosura.one>
2023-09-08 23:13:39 +0800

    bump menu.js version

--- js/menu.js
+++ js/menu.js
@@ -15,14 +15,27 @@ observer.observe(menu);
 
 // 移动端抽屉菜单
 const openMenuButton = document.getElementById("open-menu");
+const closeMenuButton = document.getElementById("close-menu");
 const drawer = document.getElementById("drawer");
 
 openMenuButton.addEventListener("click", () => {
     drawer.classList.add("open");
 });
 
+// 使用 touchend 事件处理 iOS 设备上的点击事件
+window.addEventListener("touchend", (e) => {
+    if (!drawer.contains(e.target) && !openMenuButton.contains(e.target)) {
+        drawer.classList.remove("open");
+    }
+});
+
+// 使用 click 事件处理非iOS设备上的点击事件
 window.addEventListener("click", (e) => {
     if (!drawer.contains(e.target) && !openMenuButton.contains(e.target)) {
         drawer.classList.remove("open");
     }
+});
+
+closeMenuButton.addEventListener("click", () => {
+  drawer.classList.remove("open");
 });
