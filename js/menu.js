38fade7f985ea653dc5fe826f12ba33cee16041e
Frudrax Cheng <i@cynosura.one>
2023-09-08 15:38:54 +0800

    updated menu-b layout

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
+// 菜单吸顶逻辑
 const menu = document.querySelector('.menu');
-
 const observer = new IntersectionObserver(
   ([entry]) => {
     if (entry.intersectionRatio < 1) {
@@ -12,3 +12,17 @@ const observer = new IntersectionObserver(
 );
 
 observer.observe(menu);
+
+// 移动端抽屉菜单
+const openMenuButton = document.getElementById("open-menu");
+const drawer = document.getElementById("drawer");
+
+openMenuButton.addEventListener("click", () => {
+    drawer.classList.add("open");
+});
+
+window.addEventListener("click", (e) => {
+    if (!drawer.contains(e.target) && !openMenuButton.contains(e.target)) {
+        drawer.classList.remove("open");
+    }
+});
