48d100a997fa174396d18738352f65dcce783e42
Frudrax Cheng <i@cynosura.one>
2023-09-21 18:10:02 +0800

    removed menu at-top js

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,3 @@
-// 菜单吸顶逻辑
-const menu = document.querySelector('.menu');
-const observer = new IntersectionObserver(
-  ([entry]) => {
-    if (entry.intersectionRatio < 0.99) {
-      menu.classList.add('at-top');
-    } else {
-      menu.classList.remove('at-top');
-    }
-  },
-  { threshold: [0.99] } // 修复 Edge 浏览器中的判断问题
-);
-
-observer.observe(menu);
-
 // 移动端抽屉菜单
 const openMenuButton = document.getElementById("open-menu");
 const closeMenuButton = document.getElementById("close-menu");
