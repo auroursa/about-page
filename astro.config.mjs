1bbd22df9939a00d5930f73cb238a95e3ff937e5
Frudrax Cheng <i@cynosura.one>
2026-02-07 18:01:29 +0800

    Optimize blog performance and developer experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cynosura.one',
+  integrations: [
+    expressiveCode(),
+    sitemap()
+  ]
 });
