0e5d304b6c63b035416463ad7091b34ad9e4b27b
Frudrax Cheng <i@cynosura.one>
2026-03-13 16:04:49 +0800

    Remove hero-blur

--- src/data/home-music.ts
+++ src/data/home-music.ts
@@ -6,5 +6,6 @@ export const homeMusicAppleLinks: string[] = [
     'https://music.apple.com/cn/album/%E3%81%86%E3%82%8B%E3%81%86%E3%81%B3%E3%81%A8/1611735643?i=1611736347',
     'https://music.apple.com/cn/album/%E3%83%9A%E3%83%B3%E3%82%AE%E3%83%B3-%E3%83%91%E3%83%AC%E3%83%BC%E3%83%89/1536275643?i=1536275821',
     'https://music.apple.com/cn/album/%E4%BA%8C%E4%BA%BA%E3%81%AE%E7%B5%86-%E3%82%BD%E3%83%9E%E3%83%AA%E3%81%A8%E6%A3%AE%E3%81%AE%E7%A5%9E%E6%A7%98-%E3%83%A1%E3%82%A4%E3%83%B3%E3%83%86%E3%83%BC%E3%83%9E/1506139433?i=1506139578',
-    'https://music.apple.com/cn/album/%E3%81%B5%E3%81%9F%E3%82%8A%E3%81%AE%E3%81%8D%E3%82%82%E3%81%A1%E3%81%AE%E3%81%BB%E3%82%93%E3%81%A8%E3%81%AE%E3%81%B2%E3%81%BF%E3%81%A4/493193501?i=493193506'
+    'https://music.apple.com/cn/album/%E3%81%B5%E3%81%9F%E3%82%8A%E3%81%AE%E3%81%8D%E3%82%82%E3%81%A1%E3%81%AE%E3%81%BB%E3%82%93%E3%81%A8%E3%81%AE%E3%81%B2%E3%81%BF%E3%81%A4/493193501?i=493193506',
+    'https://music.apple.com/cn/album/lost-ones-weeping/1838980670?i=1838981220'
 ];
