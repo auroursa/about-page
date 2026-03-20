52fe6adffa9c837cda011b4eeaa3d34454095c04
Frudrax Cheng <i@cynosura.one>
2026-03-20 11:41:26 +0800

    Let's fix 503 error.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://cynosura.one',
   prefetch: {
-    prefetchAll: true,
-    defaultStrategy: 'viewport',
+    defaultStrategy: 'hover',
   },
   vite: {
     plugins: [tailwindcss()],
